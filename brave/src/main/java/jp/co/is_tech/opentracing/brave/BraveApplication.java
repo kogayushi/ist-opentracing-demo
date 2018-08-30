0c2f59cb138749e4688c0a0a63197b0f951848b4
kogayushi <youshe.old.pleasure@gmail.com>
2018-08-30 23:54:00 +0900

    initial commit

---
+++ brave/src/main/java/jp/co/is_tech/opentracing/brave/BraveApplication.java
@@ -0,0 +1,12 @@
+package jp.co.is_tech.opentracing.brave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraveApplication.class, args);
+	}
+}
---
+++ sleuth/src/main/java/jp/co/is_tech/opentracing/sleuth/SleuthApplication.java
@@ -0,0 +1,12 @@
+package jp.co.is_tech.opentracing.sleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthApplication.class, args);
+	}
+}
